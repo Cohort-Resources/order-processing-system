78645b0ce1715deb83237b5a89573be26b307f84
Devin Catuns <devincatuns1@gmail.com>
2025-11-26 10:41:22 -0500

    initialize customer service

---
+++ customer-service/src/test/java/xyz/catuns/dscms/customer/CustomerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.catuns.dscms.customer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
